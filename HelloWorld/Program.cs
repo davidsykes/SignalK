7f22d7e5ab81da4651dd6045bf12349e24482910
David Sykes <david@sykes.org.uk>
2024-03-31 07:13:45 +0100

    Make a connection

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net.Http;
+
+Console.WriteLine("Hello, World!");
+
+
+
+var _httpClient = new HttpClient();
+var url = "http://192.168.1.87:3000/signalk";
+
+var response = await _httpClient.GetAsync(url);
+var responseContent = response.Content;
+using var reader = new StreamReader(await responseContent.ReadAsStreamAsync());
+var responseText = await reader.ReadToEndAsync();
+
+
+Console.WriteLine(responseText);
+
+
