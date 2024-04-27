eeabe41faf9cf70da201827705c108b99d0e0ce0
David Sykes <david@sykes.org.uk>
2024-04-27 08:06:26 +0100

    Adding application parameters to SetValue

--- net/SetValue/Program.cs
+++ net/SetValue/Program.cs
@@ -1,14 +1,43 @@
-﻿using SignalKLibrary;
+﻿using ApplicationParameters;
+using SignalKLibrary;
 
-Console.WriteLine("Hello, World!");
+var applicationParameterDefinitions = new List<ApplicationParameterDefinition>
+            {
+                new() {
+                    Name = "serverip",
+                    Type = ApplicationParameterType.RequiredParameter,
+                    UsageMessage = "Ip address of the SignalK Server"
+                },
+                new() {
+                    Name = "name",
+                    Type = ApplicationParameterType.RequiredParameter,
+                    UsageMessage = "The variable name to set"
+                },
+                new() {
+                    Name = "value",
+                    Type = ApplicationParameterType.RequiredParameter,
+                    UsageMessage = "The value to set"
+                }
+            };
+
+var manager = ApplicationParametersFactory.CreateApplicationParameters(
+    "How to use the application",
+    applicationParameterDefinitions,
+    args
+    );
+
+var serverIp = manager.GetRequiredParameter<string>("serverip");
+var name = manager.GetRequiredParameter<string>("name");
+var value = manager.GetRequiredParameter<string>("value");
+
 
 try
 {
-    var streamingUrl = await SignalKService.RetrieveStreamingEndpoint("192.168.1.87");
+    var streamingUrl = await SignalKService.RetrieveStreamingEndpoint(serverIp);
     var logger = new ConsoleLogger();
     var dataSource = await SignalKService.CreateDataSource(streamingUrl, "logger", "logger", logger);
-    var value = dataSource.CreateValue<double>("home.temperature");
-    await value.Set(3.412);
+    var settableValue = dataSource.CreateValue<string>(name);
+    await settableValue.Set(value);
     dataSource.Close();
 }
 catch (SKLibraryException ex)
